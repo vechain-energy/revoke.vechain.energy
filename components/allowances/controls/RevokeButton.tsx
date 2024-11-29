b390fb2d86a7f744b77644fff9a03e2e8c3f49d1
Mario Micklisch <mario@favo.org>
2024-11-29 08:31:55 +0100

    Add revoked confirmation in table

--- components/allowances/controls/RevokeButton.tsx
+++ components/allowances/controls/RevokeButton.tsx
@@ -3,6 +3,7 @@ import { useTransactionStore } from 'lib/stores/transaction-store';
 import { getAllowanceKey } from 'lib/utils/allowances';
 import { useTranslations } from 'next-intl';
 import Button from '../../common/Button';
+import { CheckCircleIcon as IconSuccess } from '@heroicons/react/24/outline';
 
 interface Props {
   allowance: AllowanceData;
@@ -14,6 +15,16 @@ const RevokeButton = ({ allowance, disabled, revoke }: Props) => {
   const t = useTranslations();
   const result = useTransactionStore((state) => state.results[getAllowanceKey(allowance)]);
   const loading = result?.status === 'pending';
+  const confirmed = result?.status === 'confirmed';
+
+  if (confirmed) {
+    return (
+      <Button disabled style="secondary" size="sm">
+        <IconSuccess className="w-5 h-5 mr-1" />
+        {t('common.buttons.revoked')}
+      </Button>
+    );
+  }
 
   return (
     <Button disabled={disabled} loading={loading} style="secondary" size="sm" onClick={revoke}>
